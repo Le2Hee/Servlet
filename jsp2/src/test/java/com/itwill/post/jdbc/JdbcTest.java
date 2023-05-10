e7664644be5f28761b52cee47f9600d34e9da731
Le2Hee <dlcksgml3685@naver.com>
2023-05-10 18:06:47 +0900

    05.10

--- jsp2/src/test/java/com/itwill/post/jdbc/JdbcTest.java
+++ jsp2/src/test/java/com/itwill/post/jdbc/JdbcTest.java
@@ -2,16 +2,28 @@ package com.itwill.post.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.itwill.post.model.Post;
 
 import oracle.jdbc.OracleDriver;
+
+import static com.itwill.post.model.Post.Entity.*;
 
 // JUnit Test(자바 단위 테스트)를 하기 위한 클래스.
 // JDBC(Java Database Connectivity) 테스트 - ojdbc11 라이브러리 동작 여부 테스트.
 // main()를 만들지 않고, test()를 작성하면 junit-jupiter-engine에서 test()를 실행.
+@TestMethodOrder(OrderAnnotation.class) // 테스트 메서드 실행 순서를 애너테이션을 설정한다.
 public class JdbcTest {
     
     // Oracle DB 접속 주소
@@ -36,7 +48,18 @@ public class JdbcTest {
     
     //  테스트 성공/ 실패 여부는 test() 작성자가 설정해야 된다.
     
+    // @Test 애너테이션이 2개 이면 형식상으로는 윗 코드 부터 진행하지만 
+    //  method의 실행순서를 설정할 수 있다.
+    //  -> 두 곳에서 설정을 해야된다. 
+    //  1. class
+    //   -> @TestMethodOrder(OrderAnnotation.class) 
+    //   -> 테스트 메서드 실행 순서를 애너테이션을 설정한다.
+    //  2. method
+    //   -> @Order(2)
+    //   -> method 마다 번호를 지정한다. 
+    
     @Test
+    @Order(2) // 2번째로 실행할 테스트 메서드.
     public void testSelect( ) throws SQLException {
         // 1. JDBC 라이브러리를 DriverManager에 등록.
         DriverManager.registerDriver(new OracleDriver());
@@ -51,12 +74,69 @@ public class JdbcTest {
         
         System.out.println("conn : " + conn);
         
+        // POSTS 테이블의 전체 내용을 검색(select)하고 
+        // ArrayList<Post>를 생성하고,
+        // 그 결과를 콘솔창에 출력.
+        // ArrayList<Post>의 원소 개수는 1개임을 주장.
+        String sql = "select * from posts";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        ArrayList<Post> list = new ArrayList<>();
+        int count = 0;
+        
+        while (rs.next()) {
+            long id = rs.getLong(COL_ID);
+            String title = rs.getString(COL_TITLE);
+            String content = rs.getString(COL_CONTENT);
+            String author = rs.getString(COL_AUTHOR);
+            LocalDateTime createdTime = rs.getTimestamp(COL_CREATEDTIME).toLocalDateTime();
+            LocalDateTime modifiedTime = rs.getTimestamp(COL_MODIFIEDTIME).toLocalDateTime();
+            
+            Post p = new Post(id, title, content, author, createdTime, modifiedTime);
+            
+            list.add(p);
+            count++;
+            
+            System.out.println(p);
+        }
+        
+        Assertions.assertEquals(count, list.size());
+        // -> list의 크기 (원소 개수)가 1이면 단위 테스트 성공, 그렇지 않으면 실패.
+        
         // DB와 연결된 접속을 해제
+        rs.close();
+        stmt.close();
         conn.close();
         System.out.println("연결 해제 성공");
         
     }
     
-    
+    @Test // JUnit 엔진에서 호출할 테스트 메서드.
+    @Order(1) // 1번째로 실행할 테스트 메서드.
+    public void testInsert() throws SQLException {
+        // Driver 등록 -> Connection -> PreparedStatement -> execute -> 결과처리 -> 리소스해제
+        
+        DriverManager.registerDriver(new OracleDriver());
+        
+        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        
+        String sql = "insert into posts(title, content, author) values (?, ?, ?)";
+        
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        
+        stmt.setString(1, "BUGATTI LA VOITRUE NOIRE");
+        stmt.setString(2, "$12,056,000.00");
+        stmt.setString(3, "BUGATTI");
+        
+        int result = stmt.executeUpdate();
+        Assertions.assertEquals(1, result);
+        // -> insert 문장의 실행 결과가 1이면 단위 테스트 성공, 그렇지 않으면 실패.
+        
+        stmt.close();
+        conn.close();
+        
+    }
     
 }
